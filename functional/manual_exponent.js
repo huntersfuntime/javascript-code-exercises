42d795e875a1b78c2bde3251ede3a5abfecab2b5
jordanhudgens <jordan@devcamp.com>
2018-03-20 00:03:44 -0500

    Added exercise for the manual exponent exercise

---
+++ functional/manual_exponent.js
@@ -0,0 +1,11 @@
+const toThePowerOf = (num, exp) => {
+  const items = Array(exp).fill(num);
+  const reducer = (accumulator, currentValue) => accumulator * currentValue;
+  return items.reduce(reducer);
+};
+
+toThePowerOf(2, 3); //?
+toThePowerOf(3, 3); //?
+toThePowerOf(4, 2); //?
+toThePowerOf(10, 10); //?
+10 ** 10; //?
